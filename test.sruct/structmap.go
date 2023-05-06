770e2f23596b4fc22d2b51250f3c94d68c63161a
root <root@Code01-Zhao>
2023-05-06 15:46:18 +0800

    struct map

---
+++ test.sruct/structmap.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Config struct {
+	num  int
+	link map[string]string
+}
+
+func main() {
+	config := Config{
+		num:  1,
+		link: fill(),
+	}
+
+	fmt.Println(config)
+	if val, ok := config.link["a"]; ok {
+		fmt.Println(val)
+	} else {
+		fmt.Println("not contains value a ")
+	}
+
+	// illegal:  nil map, ==> panic
+	// config.link["a"] = "aaa"
+	// fmt.Println(config.link)
+
+	// legal
+	config.link = map[string]string{
+		"a": "aaa",
+	}
+	fmt.Println(config)
+}
+
+func fill() map[string]string {
+	return map[string]string{
+		"b": "bbb",
+	}
+}
