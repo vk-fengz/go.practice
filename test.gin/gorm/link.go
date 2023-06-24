3e2fd29dcfc8a855c4a4944f96c3f3a57c7b02bd
vk-fengz <wfjmqquh@hotmail.com>
2023-06-24 15:12:47 +0800

    gin

---
+++ test.gin/gorm/link.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	dsn := "root:root@tcp(127.0.0.1:3306)/gva?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("数据库连接错误", err)
+		return
+	}
+	var tables []string
+	// 原生sql
+	db.Raw("SHOW TABLES FROM gva").Scan(&tables)
+	fmt.Println(tables)
+}
