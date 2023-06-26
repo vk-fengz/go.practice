c777ce208c322dd2f1c3e2768d432add9b558c35
vk-fengz <wfjmqquh@hotmail.com>
2023-06-26 15:04:32 +0800

    go bool

--- test.bool/trueorfalse.go
+++ test.bool/trueorfalse.go
@@ -6,6 +6,11 @@ func main() {
 
 	a := true || false
 	b := false || true
-
 	fmt.Println(a, b)
+
+	aint := 1
+	isTrue := aint == 1
+	isFalse := aint == 0
+	fmt.Println("aint值:", aint, "  等于1:", isTrue, "  等于0:", isFalse)
+
 }
