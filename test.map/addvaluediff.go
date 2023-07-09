a7a5899dff899eb5dee4c72e51e1a43db9623c00
vk-fengz <wfjmqquh@hotmail.com>
2023-07-09 22:11:59 +0800

    len map  add another

--- test.map/addvaluediff.go
+++ test.map/addvaluediff.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	books := make(map[string]string)
+	books := make(map[string]string, 2)
 	books["a"] = "aaa"
 	books["b"] = "bbb"
+	fmt.Println("The original map:")
 	fmt.Println(books)
+	books["dd"] = "dddd"
+	fmt.Println("add value after: ", books) // 突破len限制
+	fmt.Println("--------")
 
 	// 复制陷阱, 通过for range 复制到新map中
 	aBook := books
 	aBook["c"] = "ccc"
-	fmt.Println(aBook)
-	fmt.Println(books)
+	fmt.Println("copy map aBook: ", aBook)
+	fmt.Println("origin map: ", books)
 }
