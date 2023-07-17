031abf2a315b006c4341fccfa6107b63afeb0c4a
vk-fengz <wfjmqquh@hotmail.com>
2023-07-17 21:55:37 +0800

    time go

--- test.time/parse.go
+++ test.time/parse.go
@@ -9,6 +9,7 @@ func main() {
 	var layout string = "2006-01-02 15:04:05"
 	var timeStr string = "2019-12-12 15:22:12"
 	var timeOnly string = "15:22:12"
+	var R1123Z string = "Mon, 02 Jan 2006 15:04:05 +0800"
 
 	timeObj1, _ := time.Parse(layout, timeStr)
 	fmt.Println(timeObj1)
@@ -18,6 +19,7 @@ func main() {
 	tOnly, _ := time.Parse(time.TimeOnly, timeOnly)
 	fmt.Println(tOnly)
 	fmt.Println("tOnly after: ", tOnly.After(time.Now()))
+
 	// use "15:22:12" construct new time days+2
 	tNowCons := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), tOnly.Hour(), tOnly.Minute(), tOnly.Second(), 0, time.Now().Location())
 	fmt.Println("构建时间after now: ", tNowCons.After(time.Now()))
@@ -28,4 +30,21 @@ func main() {
 	ptnowcons := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()+2, tOnly.Hour(), tOnly.Minute(), tOnly.Second(), 0, time.Now().Location())
 	fmt.Println("value nowCons", ptnowcons)
 	fmt.Println("value nowCons", &ptnowcons)
+
+	//RFC1123Z
+	fmt.Println("--------- RFC1123Z")
+	rfz, _ := time.Parse(time.RFC1123Z, R1123Z)
+	fmt.Println("RFC1123Z dateTime: ", rfz.Month())
 }
+
+// 2019-12-12 15:22:12 +0000 UTC
+// 2019-12-12 15:22:12 +0800 CST
+// 0000-01-01 15:22:12 +0000 UTC
+// tOnly after:  false
+// 构建时间after now:  false
+// 构建时间 2023-07-17 15:22:12 +0800 CST
+// 2023-07-17 15:22:12 +0800 CST
+// value nowCons 2023-07-19 15:22:12 +0800 CST
+// value nowCons 2023-07-19 15:22:12 +0800 CST
+// --------- RFC1123Z
+// RFC1123Z dateTime:  January
